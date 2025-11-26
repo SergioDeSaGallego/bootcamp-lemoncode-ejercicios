4f33b5f50902767f57ef35ec515c924f75e96d1d
Sergio de Sá Gallego <sdesagallego@gmail.com>
2025-11-26 21:31:31 +0100

    reto1

--- Bootcamp Devops VI/01-contenedores/README.md
+++ Bootcamp Devops VI/01-contenedores/README.md
@@ -2,17 +2,23 @@
 
 
 ## Reto 1
+
 ### 1. Crear una red Docker para la comunicación
+#### Crear la red modo bridge
 ```bash
 docker network create challenge-net
 ```
 
 
 ### 2. Ejecutar MongoDB en un contenedor con persistencia de datos
+#### Creando los volúmenes
 ```bash
 docker volume create mongo-data
 docker volume create mongo-conf
+```
 
+#### Creando el contenedor
+```bash
 docker run -d \
     --name mongo-challenge \
     --mount type=volume,source=mongo-data,target=/data/db \
@@ -23,10 +29,34 @@ docker run -d \
     mongo:8.2.1
 ```
 ### 3. Ejecutar el backend localmente conectándose a tu nuevo MongoDB
+#### Archivo .env
 ```bash
-
+DATABASE_URL=mongodb://localhost:27017
+DATABASE_NAME=LemoncodeCourseDb
+HOST=localhost
+PORT=5000
 ```
+
+> Nota: Solo se mantiene así para la conexión con el backend en local, lo de node-stack estará diferentes según los siguientes retos.
+
 ### 4. Verificar que el CRUD funciona correctamente usando la extensión REST Client y el archivo backend/client.http del stack que hayas elegido
-```bash
+#### Con comandos de curl
 
+```bash
+host="http://localhost:5000"
+POST=$(cat << EOF
+{"name": "Contenedores I","instructor": "Gisela Torres","startDate": "2025-10-17T18:00:00Z","endDate": "2025-10-17T20:00:00Z","duration": 2,"level": "Beginner"}
+EOF
+)
+curl -X POST -H "Content-Type: application/json" -d "$POST" $host/api/classes
 ```
+
+![Imagen de respuesta con curl, reto1](imagenes/api_call_con_curl_reto1.jpg)
+
+##### En [reto_1_comandos_bash.sh](./reto1_comandos_bash.sh) están todas las API call que no sidsteis con [client.http](./node-stack/backend/client.http), las usé todas de golpe para probar la API y generar la semilla. No hace falta para el ejercicio esa parte, pero las dejé ahí. Dejo imagen de prueba también.
+
+![Imagen de respuesta con curl automatizado, reto1](imagenes/todas_las_api_call_con_curl_reto1.jpg)
+
+#### con la extensión REST Client de VS Code
+
+![Imagen con REST Client de VS Code](imagenes/api_call_con_REST_Client.jpg)
---
+++ Bootcamp Devops VI/01-contenedores/reto1_comandos_bash.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+# ✅ Crear una red Docker para la comunicación
+
+net_name=challenge-net
+container_name=mongo-challenge
+mongov_data=mongo-data
+mongov_cnf=mongo-conf
+
+if [ -z "$(docker network ls | grep $net_name)" ]; then
+    docker network create $net_name
+else
+    echo "usando red existente"
+fi
+
+# ✅ Ejecutar MongoDB en un contenedor con persistencia de datos
+
+if [ -z "$(docker volume ls | grep $mongov_data)" ]; then
+    docker volume create $mongov_data
+else
+    echo "usando volumen existente"
+fi
+
+if [ -z "$(docker volume ls | grep $mongov_cnf)" ]; then
+    docker volume create $mongov_cnf
+else
+    echo "usando volumen existente"
+fi
+
+if ! docker ps -a --format '{{.Names}}' | grep -wq $container_name ; then
+    docker run -d \
+    --name $container_name \
+    --mount type=volume,source=$mongov_data,target=/data/db \
+    --mount type=volume,source=$mongov_cnf,target=/data/configdb \
+    --memory="4g" --cpus="4" \
+    --network challenge-net \
+    -p 27017:27017 \
+    mongo:8.2.1
+
+else
+
+    echo "usando container existente"
+    docker start $container_name
+fi
+
+# ✅ Ejecutar el backend localmente conectándose a tu nuevo MongoDB
+
+cd ./node-stack/backend/
+npm install >/dev/null
+npm start &
+cd ../../
+
+sleep 5
+
+# ✅ Verificar que el CRUD funciona correctamente usando la extensión REST Client y el archivo backend/client.http del stack que hayas elegido
+
+host="http://localhost:5000"
+calls_POST=$(cat << EOF
+{"name": "Contenedores I","instructor": "Gisela Torres","startDate": "2025-10-17T18:00:00Z","endDate": "2025-10-17T20:00:00Z","duration": 2,"level": "Beginner"}
+{"name": "Contenedores II","instructor": "Gisela Torres","startDate": "2025-10-24T18:00:00Z","endDate": "2025-10-24T20:00:00Z","duration": 2,"level": "Beginner"}
+{"name": "Contenedores III","instructor": "Gisela Torres","startDate": "2025-10-31T18:00:00Z","endDate": "2025-10-31T20:00:00Z","duration": 2,"level": "Beginner"}
+{"name": "Contenedores IV","instructor": "Gisela Torres","startDate": "2025-11-07T18:00:00Z","endDate": "2025-11-07T20:00:00Z","duration": 2,"level": "Beginner"}
+{"name": "Contenedores V","instructor": "Gisela Torres","startDate": "2025-11-14T18:00:00Z","endDate": "2025-11-14T20:00:00Z","duration": 2,"level": "Beginner"}
+{"name": "Contenedores VI","instructor": "Gisela Torres","startDate": "2025-11-21T18:00:00Z","endDate": "2025-11-21T20:00:00Z","duration": 2,"level": "Beginner"}
+{"name": "Azure Web Services I","instructor": "Gisela Torres","startDate": "2026-02-20T18:00:00Z","endDate": "2026-02-20T20:00:00Z","duration": 2,"level": "Beginner"}
+{"name": "Azure Web Services II","instructor": "Gisela Torres","startDate": "2026-02-27T18:00:00Z","endDate": "2026-02-27T20:00:00Z","duration": 2,"level": "Beginner"}
+{"name": "Kubernetes AKS","instructor": "Gisela Torres","startDate": "2026-03-13T18:00:00Z","endDate": "2026-03-13T20:00:00Z","duration": 2,"level": "Beginner"}
+{"name": "SESIÓN IA I","instructor": "Gisela Torres","startDate": "2026-04-17T18:00:00Z","endDate": "2026-04-17T20:00:00Z","duration": 2,"level": "Beginner"}
+{"name": "SESIÓN IA II","instructor": "Gisela Torres","startDate": "2026-04-24T18:00:00Z","endDate": "2026-04-24T20:00:00Z","duration": 2,"level": "Beginner"}
+{"name": "SESIÓN IA III","instructor": "Gisela Torres","startDate": "2026-05-01T18:00:00Z","endDate": "2026-05-01T20:00:00Z","duration": 2,"level": "Beginner"}
+EOF
+)
+call_PUT=$(cat << EOF
+{"name": "Updated Class Name",
+  "instructor": "New Instructor",
+  "startDate": "2025-10-17T18:00:00Z",
+  "endDate": "2025-10-17T20:00:00Z",
+  "duration": 2,
+  "level": "Intermediate"}
+EOF
+)
+
+
+
+if curl $host/api/classes 2>/dev/null; then
+
+    echo "Get exitoso, lanzando las API call"
+
+    while IFS= read -r line
+    do
+        curl -X POST -H "Content-Type: application/json" -d "$line" $host/api/classes
+    done < <(printf '%s\n' "$calls_POST")
+
+    classId=$(curl -s $host/api/classes | jq -r '.[0]._id')
+    curl $host/api/classes/$classId
+    curl -X PUT -d "$call_PUT" $host/api/classes/$classId
+    curl -X DELETE $host/api/classes/$classId
+    curl  -X GET $host/api/classes
+fi 
+
+pkill -f "node app.js"
+# kill $(ps aux | grep -v grep | grep "node app.js" | tr -s ' ' | cut -d ' ' -f 2)
+
+docker stop $container_name
